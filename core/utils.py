9bc9b93fa9c976db7c8c4ff154e251178c641989
Brayan Monroy <brayansneider22@gmail.com>
2021-10-04 13:55:53 -0500

    edit

--- core/utils.py
+++ core/utils.py
@@ -1,9 +1,38 @@
+import os
 import numpy as np
+import cv2
+import random
 
 
-def mask_img(img, prcnt=0.5):
+class Mask():
+    
+    def __init__(self, mode=None, prcnt=0.5, size=(512,512)):
 
-    mask = (np.random.random(img.shape) < prcnt )*1. 
-    img = mask*img 
-    return img, mask
+        MODES = ["random", 'text', "irregular"]
+        self.mask = None
+
+        if mode not in MODES:
+            assert False, 'mode mask not valid'
+            
+        if mode == "random":
+            self.mask = (np.random.random(size) < prcnt )*1. 
+        else:
+
+            mask_dir  = os.path.join("masks", mode)
+            mask_path = os.listdir(mask_dir)
+            mask_path = random.choice(mask_path)
+            mask_path = os.path.join(mask_dir, mask_path)
+            self.mask = self.preload_mask(mask_path)
+
+    def __call__(self, img):
+        corrupted = self.mask*img 
+        return corrupted, self.mask
+
+    def preload_mask(self, path):
+        mask = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        mask = (mask > 128)*1.
+        return mask
+
+
+
     
